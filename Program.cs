6de255f25f1a30e97cc1782b8660f80cab1b0734
Dirk Freijters <dirk@freijters.nl>
2022-04-08 00:16:25 +0200

    Browsing is now only an option

--- Program.cs
+++ Program.cs
@@ -1,16 +1,19 @@
-﻿using System;
-using System.ComponentModel;
-using System.Diagnostics;
+﻿using System.Diagnostics;
+using System.IO.Pipes;
 using System.Text;
+using Microsoft.VisualBasic;
 
 namespace lorl;
 
 internal class Program
 {
     public static List<Int32> Indices = new();
-    public static Process process = new();
+    public static Process Process = new();
+    public static Boolean Browse;
+    public static Boolean Hide;
+    public static List<String> Results = new();
 
-    private static void Main(String[] args)
+    private static void Main()
     {
         while (true)
         {
@@ -20,24 +23,95 @@ internal class Program
                 Console.WriteLine("no");
                 continue;
             }
+            String[] args = input.Split(' ');
+            String str = args[0];
 
-            for (Int32 i = 0; i < input.Length; i++)
+            if (args.Length is < 1)
             {
-                Char c = input[i];
+                Console.WriteLine("no");
+                continue;
+            }
+
+            for (Int32 i=1;i < args.Length; i++)
+            {
+                switch (args[i])
+                {
+                    case "--browse":
+                    {
+                        Browse = true;
+                    }
+                    break;
+                    case "--hide":
+                    {
+                        Hide = true;
+                    }
+                    break;
+                }
+            }
+
+            for (Int32 i = 0; i < str.Length; i++)
+            {
+                Char c = str[i];
                 if (c is 'I' or 'l') Indices.Add(i);
             }
 
             if (Indices.Count > 0)
             {
-                ReplaceIL(input, 0);
-                Indices.Clear();
+                ReplaceIL(str, 0);
             }
             else
             {
-                process.StartInfo.UseShellExecute = true;
-                process.StartInfo.FileName = input;
-                process.Start();
+                Results.Add(str);
             }
+
+            if (!Hide)
+            {
+                foreach (String result in Results)
+                {
+                    Console.WriteLine(result);
+                }
+            }
+
+            if (Browse)
+            {
+                Boolean? answerGiven = null;
+                if (Results.Count <= 1)
+                {
+                    answerGiven = true;
+                }
+                else
+                {
+                    Console.WriteLine($"{Results.Count} results. Are you sure you want to open those in your browser? (y/N)");
+                    while (answerGiven == null)
+                    {
+                        input = Console.ReadLine();
+                        if (String.IsNullOrEmpty(input) || String.Equals(input, "n", StringComparison.OrdinalIgnoreCase))
+                        {
+                            answerGiven = false;
+                        }
+                        else if (String.Equals(input, "y", StringComparison.OrdinalIgnoreCase))
+                        {
+                            answerGiven = true;
+                            break;
+                        }
+                    }
+                }
+
+                if ((Boolean) answerGiven)
+                {
+                    foreach (String result in Results)
+                    {
+                        OpenUrl(result);
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Aborted");
+                }
+            }
+
+            Results.Clear();
+            Indices.Clear();
         }
     }
 
@@ -60,9 +134,21 @@ internal class Program
         }
         else
         {
-            process.StartInfo.UseShellExecute = true;
-            process.StartInfo.FileName = str;
-            process.Start();
+            Results.Add(str);
+        }
+    }
+
+    private static void OpenUrl(String str)
+    {
+        try
+        {
+            Process.StartInfo.UseShellExecute = true;
+            Process.StartInfo.FileName = str;
+            Process.Start();
+        }
+        catch
+        {
+            Console.WriteLine($"Entry \"{str}\" not recognised as URL");
         }
     }
 }
